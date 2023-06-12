71b1ed4399c24d9f45dc3e9ce135ab0deb359e83
trinhson7122 <son07012002@gmail.com>
2023-06-12 20:24:37 +0700

    Create SendMailWhenClientContactNotification.php

---
+++ app/Notifications/SendMailWhenClientContactNotification.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class SendMailWhenClientContactNotification extends Notification
+{
+    use Queueable;
+
+    private $contact;
+    public function __construct($contact)
+    {
+        $this->contact = $contact;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->subject('We received your message')
+                    ->line('Welcome ' . $this->contact->name)
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
